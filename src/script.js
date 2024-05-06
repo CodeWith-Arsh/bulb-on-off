261406de553b97628c3434337f0bb53885458eb1
Arshid Ahmad <arshid.ahmad1@gmail.com>
2024-05-06 23:25:51 +0530

    my first commit

---
+++ src/script.js
@@ -0,0 +1,3 @@
+<button onclick="document.getElementById('myImage').src='images/pic_bulbon.gif'">Turn on the light</button>
+
+<button onclick="document.getElementById('myImage').src='images/pic_bulboff.gif'">Turn off the light</button>
